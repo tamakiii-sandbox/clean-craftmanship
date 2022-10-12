a5a6b916be46749c8162ced1ba4c08e7c5290d6f
tamakiii <tamakiii@users.noreply.github.com>
2022-10-12 16:54:01 +0900

    constructor.name assertion

--- main.test.ts
+++ main.test.ts
@@ -1,5 +1,8 @@
 // url_test.ts
 import { assertEquals } from "https://deno.land/std@0.159.0/testing/asserts.ts";
+import { MyStack } from './main.ts'
 
 Deno.test("nothing", () => {
+  const stack = new MyStack
+  assertEquals(stack.constructor.name, "MyStack")
 });
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-console.log('hello')
+class MyStack {
+}
+
+export {
+  MyStack
+}
